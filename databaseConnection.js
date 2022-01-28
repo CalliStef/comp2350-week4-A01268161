d2b0947ed411320edfee92b2049ff326d19fd662
Callista Stefanie Taswin <callisto@Callistas-MacBook-Air.local>
2022-01-27 23:40:40 -0800

    databaseConnection.js updated

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,11 +4,12 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
-	multipleStatements: false
+	host: "uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "v5e4g4dmmuzx84ds",
+	password: "p3i4l8qzzkra980y",
+	database: "hjf9tox98m3vshqi",
+	multipleStatements: false,
+	reconnect: true
 };
 
 const dbConfigLocal = {
